1f82f816bacbfe28bca5b79b4b35d9f953d8dfd7
gg <1234@163.com>
2025-05-07 17:35:36 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import unocss from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [unocss(),react()],
+  plugins: [
+    unocss(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'naipindata',
+        short_name: 'np',
+        description: '奶瓶数据记录',
+      }
+    })
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
